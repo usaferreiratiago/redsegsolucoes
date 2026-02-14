568b4f53dac5f0c6c552ee2fb04a43b87e7912a7
applicationjob <usaferreiratiago@gmail.com>
2026-02-14 08:46:21 -0300

    redseg

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,7 +2,7 @@ const TopBar = () => {
   return (
     <div className="w-full bg-topbar py-2 text-center">
       <p className="text-sm text-topbar-foreground font-heading">
-        <span className="font-bold">RedSegSoluções:</span>{" "}
+        <span className="font-bold text-blue-600">RedSegSoluções:</span>{" "}
         Seu Caminho para a Excelência Digital
       </p>
     </div>
